21f759c9b3e2eed99d54f8605f8b6660d76c3cc6
David Foale <foalefamily@gmail.com>
2015-01-06 20:34:55 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Davidsname::Application.config.secret_key_base = '9c913ee6ad639e34f4c8375787bb8aa073dab4ba9ca9a5f76f27cbae0ea15011564c5637f133d5ccc421f104c9f3a95a28608facd3bc58eda2072192210637b2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Davidsname::Application.config.session_store :cookie_store, key: '_davidsname_session'
